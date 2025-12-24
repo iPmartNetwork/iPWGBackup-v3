eaa89c9ae8c6f99bc711236b1b98bee50cc14c84
ali (zeus) <131793522+iPmartNetwork@users.noreply.github.com>
2025-12-24 19:52:50 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
-apt update && apt install -y python3 python3-pip git
+apt update && apt install -y python3 python3-pip git paramiko
 pip3 install flask requests pycryptodome
 
-mkdir -p /opt/ipwgbackup/backups
-cd /opt/ipwgbackup || mkdir -p /opt/ipwgbackup
-
-cp -r ~/iPWGBackup-v2/* /opt/ipwgbackup
+mkdir -p /opt/ipwgbackup/{backups,keys,logs}
+cp -r ~/iPWGBackup-v2-professional/* /opt/ipwgbackup/
 
 systemctl daemon-reload
 systemctl enable wg_backup.service
 systemctl enable wg_backup.timer
 systemctl start wg_backup.timer
 
-echo "iPWGBackup installed and running!"
+echo "iPWGBackup Professional installed and running!"
